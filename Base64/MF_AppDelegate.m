790111b7dca3e11672c38a995a3ef6a9ba4546d6
Dave Poirier <ekscrypto@gmail.com>
2013-01-30 10:30:34 -0500

    Fix for issue #2: extra 0 were padded when padding = symbols were present
    Signed-off-by: Dave Poirier <ekscrypto@gmail.com>

--- Base64/MF_AppDelegate.m
+++ Base64/MF_AppDelegate.m
@@ -14,9 +14,31 @@
 @synthesize window = _window;
 @synthesize textField = _textField;
 
-//- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-//{
-//}
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    for( int i = 0; i < 300; i++ ) {
+        NSData *randomDataBlock = [self randomDataBlock];
+        NSString *base64Rep = [randomDataBlock base64String];
+        NSData *revertedDataBlock = [MF_Base64Codec dataFromBase64String:base64Rep];
+        if( [randomDataBlock isEqualToData:revertedDataBlock] ) {
+            NSLog(@"SUCCESS: %@", base64Rep);
+        } else {
+            NSLog(@"FAILED: %@\noriginal data block: %@\n reverted data block: %@", base64Rep, randomDataBlock, revertedDataBlock);
+        }
+    }
+}
+
+-(NSData *)randomDataBlock
+{
+#define MaxRandomDataLength 64
+    unsigned char bytes[MaxRandomDataLength];
+    long dataLength = random() % 64;
+    for(int i = 0; i < dataLength; i++ ) {
+        bytes[i] = random() % 256;
+    }
+    
+    return [[NSData alloc] initWithBytes:bytes length:dataLength];
+}
 
 -(void)encode:(id)sender
 {
